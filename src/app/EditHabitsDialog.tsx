111bc36511bdba933ba0b59ee93ea844d5b4f4e1
yennyF <fung.yenny@gmail.com>
2025-02-12 19:19:54 -0500

    feat: css changes

--- src/app/EditHabitsDialog.tsx
+++ src/app/EditHabitsDialog.tsx
@@ -60,16 +60,22 @@ function DialogContent() {
     return (
         <Dialog.Content className="content relative flex flex-col gap-10 w-[480px] max-h-full p-[30px] overflow-y-scroll bg-neutral-800">
             <Dialog.Close>
-                <Cross1Icon />
+                <div className="button-icon">
+                    <Cross1Icon />
+                </div>
             </Dialog.Close>
             <Dialog.Title>Edit habits</Dialog.Title>
             <div className="flex flex-col items-start gap-3">
                 {habits.map((habit, index) => (
-                    <div key={index} className="flex items-center justify-between gap-3 w-full">
+                    <div key={index} className="habitItem flex items-center justify-between gap-0 w-full py-1">
                         {habit}
-                        <div className="flex items-center justify-between gap-2 ">
-                            <Pencil1Icon />
-                            <TrashIcon onClick={() => handleDeleteHabitClick(habit)} />
+                        <div className="habitOptions flex items-center justify-between gap-1">
+                            <div className="button-icon">
+                                <Pencil1Icon className="icon"/>
+                            </div>
+                            <div className="button-icon">
+                                <TrashIcon className="icon" onClick={() => handleDeleteHabitClick(habit)} />
+                            </div>
                         </div>
                     </div>
                 ))}
