ae462bb68be5e695ac018903e7f5f676618fa6ae
yennyF <fung.yenny@gmail.com>
2025-02-18 17:44:53 -0500

    feat: add today highlight

--- app/CalendarList.tsx
+++ app/CalendarList.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { use, useEffect, useRef, useState } from "react";
-import { format, subMonths, eachDayOfInterval, addDays, isFuture, endOfMonth, isFirstDayOfMonth } from "date-fns";
+import { format, subMonths, eachDayOfInterval, addDays, isFuture, endOfMonth, isFirstDayOfMonth, isToday } from "date-fns";
 import { getHabitHistory, HabitHistoryType, updateHabitHitory } from "./api";
 import useScrollTo from "./hooks/useScrollBottom";
 import useOnScreen from "./hooks/useOnScreen";
@@ -48,7 +48,7 @@ export default function CalendarList() {
 
     return (
         <>
-            <div className="flex px-6 items-center justify-end fixed top-0 w-full h-16 bg-neutral-950">
+            <div className="flex px-6 items-center justify-end fixed top-0 w-full h-16 bg-neutral-950 z-10">
                 <EditHabitsDialog>
                     <button className="button-default">
                         <Pencil1Icon />
@@ -59,7 +59,7 @@ export default function CalendarList() {
 
             <div className="flex flex-col items-center">
                 {/* Header */}
-                <div className={`sticky top-16 h-16 grid bg-neutral-950`} style={{ gridTemplateColumns: `100px 100px repeat(${habits.length}, 110px` }}>
+                <div className="sticky top-16 h-16 grid bg-neutral-950 z-10" style={{ gridTemplateColumns: `100px 100px repeat(${habits.length}, 110px` }}>
                     {["", "", ...habits].map((habit, index) => (
                         <div key={index} className="flex items-center justify-center px-1">
                             <p className="text-center text-nowrap text-ellipsis overflow-hidden">{habit}</p>
@@ -69,52 +69,60 @@ export default function CalendarList() {
 
                 {/* Body */}
                 <div className="mt-16">
-                {totalMonths.map((date, index) => {
-                      const totalDays = eachDayOfInterval ({
-                        start: date,
-                        end: endOfMonth(date)
-                    });
-                    return (
-                        <div key={index} className="flex">
-                            {/* First Column: Sticky */}
-                            <div className="sticky top-[130px] w-[100px] h-fit flex flex-col items-end -z-10">
-                                <div className="text-right font-bold">
-                                    {format(date, "MMMM")}
+                    {totalMonths.map((date, index) => {
+                        const totalDays = eachDayOfInterval({
+                            start: date,
+                            end: endOfMonth(date)
+                        });
+                        return (
+                            <div key={index} className="flex">
+                                {/* First Column: Sticky */}
+                                <div className="sticky top-[130px] w-[100px] h-fit flex flex-col items-end">
+                                    <div className="text-right font-bold">
+                                        {format(date, "MMMM")}
+                                    </div>
+                                    <div className="text-right text-xs">
+                                        {format(date, "yyyy")}
+                                    </div>
                                 </div>
-                                <div className="text-right text-xs">
-                                    {format(date, "yyyy")}
-                                </div>
-                            </div>
 
-                            {/* Other Columns */}
-                            <div>
-                                {totalDays.map((day, index) => {
-                                    const track = habitHistory[day.toDateString()];
-                                    return (
-                                        <div key={index} className="grid" style={{ gridTemplateColumns: `100px repeat(${habits.length}, 110px` }}>
-                                            <div className={`grid place-items-center ${isFirstDayOfMonth(day) && "font-bold"}`}>
-                                                {format(day, "d")}
-                                            </div>
-                                            {habits.map((habit) => (
-                                                <div key={habit} className="grid place-items-center">
-                                                    {isFuture(day)
-                                                        ? undefined
-                                                        : <button
-                                                            className={`rounded-full w-4 h-4 ${track?.[habit] === true ? "bg-amber-100" : "bg-neutral-800"} hover:bg-neutral-600 transition-all`}
-                                                            onClick={() => toogleHabit(day, habit)}
-                                                        ></button>
-                                                    }
+                                {/* Other Columns */}
+                                <div>
+                                    {totalDays.map((day, index) => {
+                                        const track = habitHistory[day.toDateString()];
+                                        const istoday = isToday(day);
+                                        return (
+                                            <div key={index} className="relative">
+                                                {istoday &&
+                                                    <div className="absolute top-0 -left-[40px] font-bold text-rose-500">
+                                                        Today
+                                                    </div>
+                                                }
+                                                <div key={index} className="grid" style={{ gridTemplateColumns: `100px repeat(${habits.length}, 110px` }}>
+                                                    <div className={`grid place-items-center ${(isFirstDayOfMonth(day) || istoday) && "font-bold"} ${istoday && "text-rose-500"}`}>
+                                                        {format(day, "d")}
+                                                    </div>
+                                                    {habits.map((habit) => (
+                                                        <div key={habit} className="grid place-items-center">
+                                                            {isFuture(day)
+                                                                ? undefined
+                                                                : <button
+                                                                    className={`rounded-full w-4 h-4 ${track?.[habit] === true ? "bg-amber-100" : "bg-neutral-800"} hover:bg-neutral-600 transition-all`}
+                                                                    onClick={() => toogleHabit(day, habit)}
+                                                                ></button>
+                                                            }
+                                                        </div>
+                                                    ))}
+                                                    {/* Add more columns as needed */}
                                                 </div>
-                                            ))}
-                                            {/* Add more columns as needed */}
-                                        </div>
-                                    )
-                                })}
-                                {/* Add more rows as needed */}
+                                            </div>
+                                        )
+                                    })}
+                                    {/* Add more rows as needed */}
+                                </div>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
                 </div>
 
                 <div ref={scrollTarget} />
