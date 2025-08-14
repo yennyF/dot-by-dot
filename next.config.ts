0d314bedcf66d2350e14d91b37b18e4c9b7eb086
yennyF <fung.yenny@gmail.com>
2025-08-14 18:02:21 -0400

    test: env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    DB_NAME: process.env.DB_NAME,
+  },
   output: "export",
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
