5e451c2d3cffb9dc8afff6596d6006e7e994f1db
yennyF <fung.yenny@gmail.com>
2025-08-14 14:55:42 -0400

    test: deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.BASE_PATH || "",
   output: "export",
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
