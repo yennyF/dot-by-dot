1a3ddeb3028e7593539a9a02f4b497d1e0423094
yennyF <fung.yenny@gmail.com>
2025-08-14 15:59:42 -0400

    Revert "test: deploy"
    This reverts commit 5e451c2d3cffb9dc8afff6596d6006e7e994f1db.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: process.env.BASE_PATH || "",
   output: "export",
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
