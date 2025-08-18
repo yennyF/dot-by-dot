eaca565be7c1057e1ad79880d4da38e6b04feb54
yennyF <fung.yenny@gmail.com>
2025-08-17 21:37:37 -0400

    feat: remove click from group

--- app/home/CalendarDay/Body/GroupRowItem.tsx
+++ app/home/CalendarDay/Body/GroupRowItem.tsx
@@ -2,10 +2,8 @@
 
 import { useTrackStore } from "@/app/stores/TrackStore";
 import { Group } from "@/app/repositories/types";
-import { LockClosedIcon } from "@radix-ui/react-icons";
 import { addDays, isToday } from "date-fns";
 import clsx from "clsx";
-import { useState } from "react";
 import { midnightUTCstring } from "@/app/util";
 import { useTaskStore } from "@/app/stores/TaskStore";
 
@@ -15,9 +13,6 @@ interface GroupRowItemProps {
 }
 
 export default function GroupRowItem({ date, group }: GroupRowItemProps) {
-  const addTracks = useTrackStore((s) => s.addTracks);
-  const deleteTracks = useTrackStore((s) => s.deleteTracks);
-
   const tasks = useTaskStore((s) => s.tasksByGroup?.[group.id]) || [];
   const taskIdSet = new Set(tasks.map((t) => t.id));
 
@@ -40,15 +35,6 @@ export default function GroupRowItem({ date, group }: GroupRowItemProps) {
 
   const isTodayDate = isToday(date);
 
-  const handleClick = () => {
-    const taskIds = Array.from(taskIdSet);
-    if (isActive) {
-      deleteTracks(date, taskIds);
-    } else {
-      addTracks(date, taskIds);
-    }
-  };
-
   return (
     <div
       className={clsx(
@@ -63,55 +49,25 @@ export default function GroupRowItem({ date, group }: GroupRowItemProps) {
         <div className="absolute left-[50%] right-0 z-[-1] h-4 animate-fade-in bg-[var(--green-5)] opacity-0" />
       )}
 
-      {isTodayDate ? (
-        <Dot isActive={isActive} onClick={handleClick} />
-      ) : (
-        <LockDot isActive={isActive} onClick={handleClick} />
-      )}
+      <Dot isActive={isActive} />
     </div>
   );
 }
 
-interface DotProps extends React.ComponentProps<"button"> {
+interface DotProps extends React.ComponentProps<"div"> {
   isActive: boolean;
 }
 
 function Dot({ isActive, ...props }: DotProps) {
   return (
-    <button
+    <div
       {...props}
       className={clsx(
-        "h-4 w-4 transform rounded-full transition-transform duration-100 hover:scale-110 active:scale-90",
-        isActive
-          ? "bg-[var(--green)]"
-          : "bg-[var(--gray)] hover:bg-[var(--green-5)]"
+        "h-4 w-4 transform rounded-full",
+        isActive ? "bg-[var(--green)]" : "bg-[var(--gray)]"
       )}
     >
       {/* {isActive && <CheckIcon className="text-white" />} */}
-    </button>
-  );
-}
-
-function LockDot({ onClick, ...props }: DotProps) {
-  const [unlock, setUnlock] = useState<boolean>();
-
-  return (
-    <div className="group relative flex justify-center">
-      {!unlock && (
-        <LockClosedIcon className="absolute -top-full h-[11px] w-[11px] text-gray-600 opacity-0 transition-opacity group-hover:opacity-100" />
-      )}
-      <Dot
-        {...props}
-        onClick={(e) => {
-          if (unlock) onClick?.(e);
-        }}
-        onMouseEnter={() => {
-          setUnlock(useTrackStore.getState().unlock);
-        }}
-        onMouseLeave={() => {
-          setUnlock(undefined);
-        }}
-      />
     </div>
   );
 }
