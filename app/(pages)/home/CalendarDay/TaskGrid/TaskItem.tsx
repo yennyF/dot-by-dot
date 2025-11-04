ef346ad967dc8ededadab241e9ca3528bc167f16
yennyF <fung.yenny@gmail.com>
2025-11-04 17:48:01 -0500

    feat: highlight weekend dots

--- app/(pages)/home/CalendarDay/TaskGrid/TaskItem.tsx
+++ app/(pages)/home/CalendarDay/TaskGrid/TaskItem.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { addDays, isToday } from "date-fns";
+import { addDays, isToday, isWeekend } from "date-fns";
 import clsx from "clsx";
 import { useTaskLogStore } from "@/app/stores/taskLogStore";
 import { CheckIcon } from "@radix-ui/react-icons";
@@ -23,60 +23,18 @@ export default function TaskItem({ date, task }: TaskItemProps) {
   );
 
   const isTodayDate = isToday(date);
+  const isWeekendDate = isWeekend(date);
 
-  return (
-    <div
-      className={clsx(
-        "app-TaskRowItem relative flex h-row w-day items-center justify-center",
-        isTodayDate && "isToday"
-      )}
-    >
-      {isPrevActive && isActive && (
-        <div
-          className={clsx(
-            "absolute left-0 right-[50%] z-[-1] h-3.5 animate-fade-in",
-            task.groupId ? "bg-[var(--accent-4)]" : "bg-[var(--accent-4)]"
-          )}
-        />
-      )}
-
-      {isNextActive && isActive && (
-        <div
-          className={clsx(
-            "absolute left-[50%] right-0 z-[-1] h-3.5 animate-fade-in",
-            task.groupId ? "bg-[var(--accent-4)]" : "bg-[var(--accent-4)]"
-          )}
-        />
-      )}
-      <Dot
-        id={task.id}
-        date={date}
-        isActive={isActive}
-        isTodayDate={isTodayDate}
-      />
-    </div>
-  );
-}
-
-function Dot({
-  id,
-  date,
-  isActive,
-  isTodayDate,
-}: {
-  id: string;
-  date: Date;
-  isActive: boolean;
-  isTodayDate: boolean;
-}) {
-  const className = clsx(
+  const classNameDot = clsx(
     "app-Dot group box-border flex size-[var(--dot-size)] items-center justify-center rounded-full transition-transform duration-100",
-    "hover:scale-110 hover:border-[1px] hover:border-black",
+    "hover:scale-110 hover:bg-[var(--accent-5)] hover:border-[1px] hover:border-black",
     "active:scale-90",
+    isActive && "!bg-[var(--accent)]",
     isTodayDate
-      ? "bg-[var(--background)] border-[1px] border-black hover:bg-[var(--accent-5)]"
-      : "bg-[var(--gray)] hover:bg-[var(--accent-5)]",
-    isActive && "!bg-[var(--accent)]"
+      ? "bg-[var(--background)] border-[1px] border-black "
+      : isWeekendDate
+        ? "bg-[var(--gray-5)]"
+        : "bg-[var(--gray)] "
   );
 
   const classNameIcon = clsx(
@@ -86,13 +44,32 @@ function Dot({
   );
 
   return (
-    <div
-      data-task-id={id}
-      data-date={date}
-      data-active={isActive}
-      className={className}
-    >
-      <CheckIcon className={classNameIcon} />
+    <div className="app-TaskRowItem relative flex h-row w-day items-center justify-center">
+      {isPrevActive && isActive && (
+        <div
+          className={clsx(
+            "absolute left-0 right-[50%] z-[-1] h-3.5 animate-fade-in",
+            task.groupId ? "bg-[var(--accent-4)]" : "bg-[var(--accent-4)]"
+          )}
+        />
+      )}
+      {isNextActive && isActive && (
+        <div
+          className={clsx(
+            "absolute left-[50%] right-0 z-[-1] h-3.5 animate-fade-in",
+            task.groupId ? "bg-[var(--accent-4)]" : "bg-[var(--accent-4)]"
+          )}
+        />
+      )}
+
+      <div
+        data-task-id={task.id}
+        data-date={date}
+        data-active={isActive}
+        className={classNameDot}
+      >
+        <CheckIcon className={classNameIcon} />
+      </div>
     </div>
   );
 }
